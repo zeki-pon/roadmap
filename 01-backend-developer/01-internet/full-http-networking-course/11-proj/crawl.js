42c3d0a004a63cbf7519269283f1c3e62288e522
Shosei-Imazeki <mon.sho.de@gmail.com>
2024-07-11 19:45:29 +0900

    webcrawlerhttp proj TDDの練習　ステップ2 関数のテストを作成

--- 01-backend-developer/01-internet/full-http-networking-course/11-proj/crawl.js
+++ 01-backend-developer/01-internet/full-http-networking-course/11-proj/crawl.js
@@ -1,3 +1,12 @@
+/* 
+  https://boot.dev -> boot.dev
+  http://boot.dev  -> boot.dev
+  https://Boot.dev -> boot.dev
+*/
 function normalizeURL(urlString) {
     return urlString;
+}
+
+module.exports = {
+    normalizeURL
 }
---
+++ 01-backend-developer/01-internet/full-http-networking-course/11-proj/crawl.test.js
@@ -0,0 +1,9 @@
+const { normalizeURL } = require('./crawl.js');
+const { test, expect } = require('@jest/globals');
+
+test('normalizeURL strip protocol', () => {
+    const input = 'https://blog.boot.dev/path';
+    const actual = normalizeURL(input);
+    const expected = 'blog.boot.dev/path';
+    expect(actual).toEqual(expected);
+})
