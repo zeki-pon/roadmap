697d153de0d6ae0e7050ea6fae89ca8e30216626
Shosei-Imazeki <mon.sho.de@gmail.com>
2024-07-13 07:33:30 +0900

    webcrawlerhttp proj crawl single page with error handling  4:45

--- 01-backend-developer/01-internet/full-http-networking-course/11-proj/crawl.js
+++ 01-backend-developer/01-internet/full-http-networking-course/11-proj/crawl.js
@@ -1,5 +1,33 @@
 const { JSDOM } = require('jsdom')
 
+async function crawlPage(currentURL) {
+    console.log(`actively crawling: ${currentURL}`);
+
+    try {
+        const resp = await fetch(currentURL);
+
+        if (resp.status > 399) {
+            // test command: npm run start https://wagslane.dev/garbagepath
+            console.log(`error in fetch with status code: ${resp.status} on page: ${currentURL}`);
+            return;
+        }
+
+        const contentType = resp.headers.get("content-type");
+        if (!contentType.includes("text/html")) {
+            // test command: npm run start https://wagslane.dev/sitemap.xml
+            console.log(`non html response, content type: ${contentType} on page: ${currentURL}`);
+            return;
+        }
+
+        // htmlとしてフォーマットしたいので.text()
+        console.log(await resp.text());
+    } catch (error) {
+        console.log(`error in fetch: ${error.message}, on page: ${currentURL}`);
+    }
+
+
+}
+
 function getURLsFromHTML(htmlBody, baseURL) {
     const urls = [];
     const dom = new JSDOM(htmlBody);
@@ -42,5 +70,6 @@ function normalizeURL(urlString) {
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 }
--- 01-backend-developer/01-internet/full-http-networking-course/11-proj/main.js
+++ 01-backend-developer/01-internet/full-http-networking-course/11-proj/main.js
@@ -1,1 +1,22 @@
-console.log("Hello World");
+const { crawlPage } = require('./crawl.js')
+
+function main() {
+    // 最初の引数はプログラムの名前
+    // 2番目の引数はコードまたはエントリポイントファイルの名前
+    // そのため、3という数値が出てくる
+    if (process.argv.length < 3) {
+        console.log("no website provided");
+        process.exit(1);
+    }
+
+    if (process.argv.length > 3) {
+        console.log("too many command line args");
+        process.exit(1);
+    }
+    const baseURL = process.argv[2];
+
+    console.log(`starting crawl or ${baseURL}`);
+    crawlPage(baseURL);
+}
+
+main()
