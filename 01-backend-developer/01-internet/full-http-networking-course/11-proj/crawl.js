6d1a880e40297bdc379970e724851b3399a2506e
Shosei-Imazeki <mon.sho.de@gmail.com>
2024-07-12 19:53:11 +0900

    webcrawlerhttp proj HTML文字列からURLを取得する関数を追加　4:27

--- 01-backend-developer/01-internet/full-http-networking-course/11-proj/crawl.js
+++ 01-backend-developer/01-internet/full-http-networking-course/11-proj/crawl.js
@@ -1,3 +1,31 @@
+const { JSDOM } = require('jsdom')
+
+function getURLsFromHTML(htmlBody, baseURL) {
+    const urls = [];
+    const dom = new JSDOM(htmlBody);
+    const linkElements = dom.window.document.querySelectorAll('a');
+    for (const linkElement of linkElements) {
+        if (linkElement.href.slice(0, 1) === '/') {
+            // relative
+            try {
+                const urlObj = new URL(`${baseURL}${linkElement.href}`);
+                urls.push(urlObj.href);
+            } catch (error) {
+                console.log(`error with relative url: ${error.message}`)
+            }
+        } else {
+            // absolute
+            try {
+                const urlObj = new URL(linkElement.href);
+                urls.push(urlObj.href);
+            } catch (error) {
+                console.log(`error with absolute url: ${error.message}`);
+            }
+        }
+    }
+    return urls;
+}
+
 /* 
   https://boot.dev -> boot.dev
   http://boot.dev  -> boot.dev
@@ -13,5 +41,6 @@ function normalizeURL(urlString) {
 }
 
 module.exports = {
-    normalizeURL
+    normalizeURL,
+    getURLsFromHTML
 }
