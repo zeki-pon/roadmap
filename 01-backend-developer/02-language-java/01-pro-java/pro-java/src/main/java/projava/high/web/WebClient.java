be62a4e096b92d9b47bc88b465bc8d8e582eee30
Shosei-Imazeki <mon.sho.de@gmail.com>
2024-07-17 19:41:00 +0900

    WebClientをHTTPSで通信するように修正

--- 01-backend-developer/02-language-java/01-pro-java/pro-java/src/main/java/projava/high/web/WebClient.java
+++ 01-backend-developer/02-language-java/01-pro-java/pro-java/src/main/java/projava/high/web/WebClient.java
@@ -1,10 +1,11 @@
 package projava.high.web;
 
+import javax.net.SocketFactory;
+import javax.net.ssl.SSLSocketFactory;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
-import java.net.Socket;
 
 /**
  * 対象ドメインの80番ポートに接続して3行送信
@@ -12,16 +13,18 @@ import java.net.Socket;
  */
 public class WebClient {
     public static void main(String[] args) throws IOException {
-        var domain = "example.com";
-        try (var soc = new Socket(domain, 80);
+        var domain = "www.google.com";
+
+        SocketFactory factory = SSLSocketFactory.getDefault();
+        try (var soc = factory.createSocket(domain, 443);
              var pw = new PrintWriter(soc.getOutputStream());
              var isr = new InputStreamReader(soc.getInputStream());
              var bur = new BufferedReader(isr))
         {
             pw.println("GET /index.html HTTP/1.1");
             pw.println("Host: " + domain);
-            pw.println();
-            pw.flush();
+            pw.println(); // 空行を送るとリクエストは終了、レスポンス待ちとなる
+            pw.flush();  // 強制的に送信
             bur.lines()
                     .limit(20)
                     .forEach(System.out::println);
